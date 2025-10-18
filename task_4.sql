0498152132c39310e60303711933d0698ad1d0ed
ayomijpeg <ayomitomiwa53@gmail.com>
2025-10-19 00:01:09 +0100

    Update4a

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,17 @@
--- task_4.sql
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_schema = "alx_book_store" AND table_name = "books";
+-- A script that prints the full description of the table 'books'
+-- from the database 'alx_book_store' without using DESCRIBE or EXPLAIN.
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+
