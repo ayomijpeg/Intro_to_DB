e194e2d6d912401b1ed49d3ba8a11da6f66f5c4e
ayomijpeg <ayomitomiwa53@gmail.com>
2025-10-19 00:03:34 +0100

    Update4b

--- task_4.sql
+++ task_4.sql
@@ -3,15 +3,5 @@
 
 USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+SHOW FULL COLUMNS FROM books;
 
