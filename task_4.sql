7829d1039a3d59f4f266acf5990e0630274b7237
ayomijpeg <ayomitomiwa53@gmail.com>
2025-10-18 23:58:59 +0100

    Update4

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,4 @@
--- This script prints the full description of the 'books' table.
--- It retrieves column information from the INFORMATION_SCHEMA.
--- The database name is passed as an argument to the mysql command.
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+-- task_4.sql
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.columns
+WHERE table_schema = "alx_book_store" AND table_name = "books";
